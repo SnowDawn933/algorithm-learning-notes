de9731e062160463047528b13ba22aab7b1c0a7b
SnowDawn933 <1974338433@qq.com>
2024-01-06 21:36:58 +0800

    add dog funcationality

--- animal.py
+++ animal.py
@@ -2,7 +2,13 @@ import sys
 def default():
     print('hello')
 
+def dog():
+    print('Woof!wangwang!')
+
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
 if __name__ == '__main__':
     main()
